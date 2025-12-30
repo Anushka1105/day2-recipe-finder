f9fc0123f91e34e1cff7d1fe79eeb2ee82c8e2bb
Ritika Yadav <ritikayadav2412@gmail.com>
2025-12-30 17:25:06 +0530

    added url

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -13,9 +13,9 @@ function App() {
     const fetchRecipes = async () => {
         setLoading(true)
         try {
-            const response = await fetch('http://localhost:5000/api/recipes')
+            const response = await fetch('http://localhost:5000/api/get')
             const data = await response.json()
-            setRecipes(data)
+            setRecipes(data.data)
         } catch (error) {
             console.error('Error fetching recipes:', error)
         } finally {
@@ -88,7 +88,7 @@ function App() {
                             <div key={recipe.id} className="card" style={{ margin: 0 }}>
                                 <img className='recipe-img' src="https://images.unsplash.com/photo-1609159085820-d9a6ff0f469e?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
                                 <h3>{recipe.name}</h3>
-                                <p><strong>Ingredients:</strong> {recipe.ingredients}</p>
+                                <p><strong>Meal Type:</strong> {recipe.mealType}</p>
                                 {/* <p><strong>Instructions:</strong> {recipe.instructions}</p> */}
                                 <div className='fav'>
                                     <p>5 <svg className='star' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(240,187,64,1)"><path d="M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26Z"></path></svg></p>
@@ -109,7 +109,7 @@ function App() {
             </div>
             
 
-                {/* {favorites.length > 0 && (
+                {favorites.length > 0 && (
                 <div className="card">
                     <h2>Favorites ({favorites.length})</h2>
                     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))', gap: '15px', marginTop: '15px' }}>
@@ -124,7 +124,7 @@ function App() {
                         ))}
                     </div>
                 </div>
-            )} */}
+            )}
 
         </div>
     )
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './index.css'
+import './index.css'
 import Detailed from './Detailed.jsx'
-import './Detailed.css'
+// import './Detailed.css'
 import Fav from './Fav.jsx'
 import './Fav.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        {/* <App /> */}
-        <Detailed />
+        <App />
+        {/* <Detailed /> */}
         {/* <Fav /> */}
         
     </React.StrictMode>,
